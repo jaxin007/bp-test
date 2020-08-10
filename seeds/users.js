f321c0ab57b5b0fbe57355c3dfb85cac988ef670
Dima Bilozor <swatdimonbil@gmail.com>
2020-08-10 14:54:07 +0300

    bp-test-5 added seeds

---
+++ seeds/users.js
@@ -0,0 +1,23 @@
+const knex = require('knex');
+
+// eslint-disable-next-line no-shadow
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(() => {
+      const seedUsers = [
+        {
+          email: 'dima@gmail.com',
+          password: '123',
+        },
+        {
+          phone_number: '38096333222',
+          password: '123',
+        },
+      ];
+      // Inserts seed entries
+      return knex('users').insert([
+        ...seedUsers,
+      ]);
+    });
+};
